d75cfa7ed171d54552ba85cfa4454b83e6669817
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-12-05 05:23:14 +0100

    [Back] Picture factory using unsplash links

--- back/database/factories/PictureFactory.php
+++ back/database/factories/PictureFactory.php
@@ -16,8 +16,31 @@ class PictureFactory extends Factory
      */
     public function definition(): array
     {
+        $collection = [
+            'https://images.unsplash.com/photo-1618773928121-c32242e63f39',
+            'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa',
+            'https://images.unsplash.com/photo-1549294413-26f195200c16',
+            'https://images.unsplash.com/photo-1445019980597-93fa8acb246c',
+            'https://images.unsplash.com/photo-1566073771259-6a8506099945',
+            'https://images.unsplash.com/photo-1611892440504-42a792e24d32',
+            'https://images.unsplash.com/photo-1455587734955-081b22074882',
+            'https://images.unsplash.com/photo-1496417263034-38ec4f0b665a',
+            'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb',
+            'https://images.unsplash.com/photo-1495365200479-c4ed1d35e1aa',
+            'https://images.unsplash.com/photo-1445991842772-097fea258e7b',
+            'https://images.unsplash.com/photo-1584132967334-10e028bd69f7',
+            'https://images.unsplash.com/photo-1582719508461-905c673771fd',
+            'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4',
+            'https://images.unsplash.com/photo-1596701062351-8c2c14d1fdd0',
+            'https://images.unsplash.com/photo-1564501049412-61c2a3083791',
+            'https://images.unsplash.com/photo-1506059612708-99d6c258160e',
+            'https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8'
+        ];
+
         return [
-            //
+            'file_path' => $collection[array_rand($collection)],
+            'file_size' => 0,
+            'index' => 0
         ];
     }
 }
--- back/database/seeders/HotelSeeder.php
+++ back/database/seeders/HotelSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Hotel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Picture;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class HotelSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class HotelSeeder extends Seeder
      */
     public function run(): void
     {
-        Hotel::factory()->count(10)->create();
+        Hotel::factory()->count(10)->create()->each(function (Hotel $hotel) {
+            Picture::factory()
+                ->count(3)
+                ->sequence(
+                    ['index' => 0],
+                    ['index' => 1],
+                    ['index' => 2]
+                )
+                ->for($hotel)
+                ->create();
+        });
     }
 }
