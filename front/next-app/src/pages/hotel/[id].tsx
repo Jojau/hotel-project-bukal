e61c35fbb1868b6523cd02d74f340d3348258d25
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-12-06 20:38:01 +0100

    [Front] Details page - Breadcrumb & center layout adjustments

--- front/next-app/src/pages/hotel/[id].tsx
+++ front/next-app/src/pages/hotel/[id].tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, FormatNumber, Group, Heading, Stack, Text, Carousel, IconButton, Image } from '@chakra-ui/react';
+import { Box, Button, Container, FormatNumber, Group, Heading, Stack, Text, Carousel, IconButton, Image, Breadcrumb, Center } from '@chakra-ui/react';
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu"
 import { useRouter } from 'next/router'
 
@@ -24,106 +24,127 @@ export default function Page({ hotel }) {
   }
 
   return (
-    <Stack gap={'48px'} margin={'48px'}>
-      <Heading size={'4xl'} textAlign={'center'}>{hotel.name}</Heading>
+    <Center>
+      <Stack gap={'48px'} margin={'48px'} maxWidth={'8xl'}>
+        {/* Hotel name & breadcrumb */}
+        <Container gapY={'12px'}>
+          <Heading size={'4xl'} textAlign={'center'}>{hotel.name}</Heading>
 
-      {/* Hotel pictures carousel */}
-      {pictures && pictures.length > 0 && (
-        <Carousel.Root slideCount={pictures.length} allowMouseDrag={true} autoplay={true} loop={true}  >
-          <Carousel.Control justifyContent="center" gap="4" width="full">
-            <Carousel.PrevTrigger asChild>
-              <IconButton size="xs" variant="outline">
-                <LuChevronLeft />
-              </IconButton>
-            </Carousel.PrevTrigger>
+          <Center>
+            <Breadcrumb.Root>
+              <Breadcrumb.List>
+                <Breadcrumb.Item>
+                  <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+                </Breadcrumb.Item>
+                <Breadcrumb.Separator />
+                <Breadcrumb.Item>
+                  <Breadcrumb.CurrentLink>{hotel.name}</Breadcrumb.CurrentLink>
+                </Breadcrumb.Item>
+              </Breadcrumb.List>
+            </Breadcrumb.Root>
+          </Center>
+        </Container>
 
-            <Carousel.ItemGroup width="full">
-              {pictures.map((item, index) => (
-                <Carousel.Item key={index} index={index}>
-                  <Image
-                    aspectRatio="16/9"
-                    src={item.file_path}
-                    alt={'Picture '+index+' of hotel '+hotel.name}
-                    w="100%"
-                    h="100%"
-                    objectFit="contain"
-                  />
-                </Carousel.Item>
-              ))}
-            </Carousel.ItemGroup>
+        {/* Hotel pictures carousel */}
+        {pictures && pictures.length > 0 && (
+          <Center>
+            <Carousel.Root slideCount={pictures.length} allowMouseDrag={true} autoplay={true} loop={true} maxWidth={'6xl'} >
+              <Carousel.Control justifyContent="center" gap="4" width="full">
+                <Carousel.PrevTrigger asChild>
+                  <IconButton size="xs" variant="outline">
+                    <LuChevronLeft />
+                  </IconButton>
+                </Carousel.PrevTrigger>
 
-            <Carousel.NextTrigger asChild>
-              <IconButton size="xs" variant="outline">
-                <LuChevronRight />
-              </IconButton>
-            </Carousel.NextTrigger>
-          </Carousel.Control>
+                <Carousel.ItemGroup width="full">
+                  {pictures.map((item, index) => (
+                    <Carousel.Item key={index} index={index}>
+                      <Image
+                        aspectRatio="16/9"
+                        src={item.file_path}
+                        alt={'Picture ' + index + ' of hotel ' + hotel.name}
+                        w="100%"
+                        h="100%"
+                        objectFit="contain"
+                      />
+                    </Carousel.Item>
+                  ))}
+                </Carousel.ItemGroup>
 
-          <Carousel.IndicatorGroup>
-            {pictures.map((item, index) => (
-              <Carousel.Indicator
-                key={index}
-                index={index}
-                unstyled
-                _current={{
-                  outline: "2px solid currentColor",
-                  outlineOffset: "2px",
-                }}
-              >
-                <Image
-                  w="20"
-                  aspectRatio="16/9"
-                  src={item.file_path}
-                  alt={'Picture '+index+' of hotel '+hotel.name}
-                  objectFit="cover"
-                />
-              </Carousel.Indicator>
-            ))}
-          </Carousel.IndicatorGroup>
-        </Carousel.Root>
-      )}
+                <Carousel.NextTrigger asChild>
+                  <IconButton size="xs" variant="outline">
+                    <LuChevronRight />
+                  </IconButton>
+                </Carousel.NextTrigger>
+              </Carousel.Control>
 
-      {/* Content */}
-      <Stack direction={'row'} gap={'24px'}>
-        {/* Left column */}
-        <Stack flexBasis={'70%'} gap={'24px'}>
-          <Text>{hotel.description}</Text>
+              <Carousel.IndicatorGroup>
+                {pictures.map((item, index) => (
+                  <Carousel.Indicator
+                    key={index}
+                    index={index}
+                    unstyled
+                    _current={{
+                      outline: "2px solid currentColor",
+                      outlineOffset: "2px",
+                    }}
+                  >
+                    <Image
+                      w="20"
+                      aspectRatio="16/9"
+                      src={item.file_path}
+                      alt={'Picture ' + index + ' of hotel ' + hotel.name}
+                      objectFit="cover"
+                    />
+                  </Carousel.Indicator>
+                ))}
+              </Carousel.IndicatorGroup>
+            </Carousel.Root>
+          </Center>
+        )}
 
-          {/* Admin actions box */}
-          <Box borderWidth={'1px'} padding={4}>
-            <Heading size={'lg'}>Admin actions</Heading>
-            <Group>
-              <Button onClick={() => router.push(`/hotel/${hotel.id}/edit`)}>Edit hotel</Button>
-              <Button colorPalette={'red'} onClick={deleteFunction}>Delete hotel</Button>
-            </Group>
-          </Box>
-        </Stack>
+        {/* Content */}
+        <Stack direction={'row'} gap={'24px'}>
+          {/* Left column */}
+          <Stack flexBasis={'70%'} gap={'24px'}>
+            <Text>{hotel.description}</Text>
 
-        {/* Right column */}
-        <Stack flexBasis={'30%'} gap={'24px'}>
-          {/* Adress box */}
-          <Box borderWidth={'1px'} padding={'12px'}>
-            <Heading size={'lg'}>Address</Heading>
-            <Text>{hotel.address}</Text>
-            {hotel.address2 ?? <Text>{hotel.address2}</Text>}
-            <Text>{hotel.zipcode} {hotel.city}, {hotel.country}</Text>
-          </Box>
+            {/* Admin actions box */}
+            <Box borderWidth={'1px'} padding={4}>
+              <Heading size={'lg'}>Admin actions</Heading>
+              <Group>
+                <Button onClick={() => router.push(`/hotel/${hotel.id}/edit`)}>Edit hotel</Button>
+                <Button colorPalette={'red'} onClick={deleteFunction}>Delete hotel</Button>
+              </Group>
+            </Box>
+          </Stack>
 
-          {/* Capacity & Price box */}
-          <Stack gapY={'12px'} borderWidth={'1px'} padding={'12px'}>
-            <Container fluid padding={0}>
-              <Heading size={'lg'}>Maximum capacity</Heading>
-              <FormatNumber value={hotel.max_capacity}></FormatNumber>
-            </Container>
+          {/* Right column */}
+          <Stack flexBasis={'30%'} gap={'24px'}>
+            {/* Adress box */}
+            <Box borderWidth={'1px'} padding={'12px'}>
+              <Heading size={'lg'}>Address</Heading>
+              <Text>{hotel.address}</Text>
+              {hotel.address2 ?? <Text>{hotel.address2}</Text>}
+              <Text>{hotel.zipcode} {hotel.city}, {hotel.country}</Text>
+            </Box>
 
-            <Container fluid padding={0}>
-              <Heading size={'lg'}>Price per night</Heading>
-              <FormatNumber value={hotel.price_per_night} style='currency' currency='USD'></FormatNumber>
-            </Container>
+            {/* Capacity & Price box */}
+            <Stack gapY={'12px'} borderWidth={'1px'} padding={'12px'}>
+              <Container fluid padding={0}>
+                <Heading size={'lg'}>Maximum capacity</Heading>
+                <FormatNumber value={hotel.max_capacity}></FormatNumber>
+              </Container>
+
+              <Container fluid padding={0}>
+                <Heading size={'lg'}>Price per night</Heading>
+                <FormatNumber value={hotel.price_per_night} style='currency' currency='USD'></FormatNumber>
+              </Container>
+            </Stack>
           </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </Center>
   )
 }
 
