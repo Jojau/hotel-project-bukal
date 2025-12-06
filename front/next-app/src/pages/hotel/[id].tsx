ad6787fc6e7a3b8276e439c17e2d0ef78d7fd08a
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-12-06 20:09:02 +0100

    [Front] Details page layout

--- front/next-app/src/pages/hotel/[id].tsx
+++ front/next-app/src/pages/hotel/[id].tsx
@@ -1,8 +1,9 @@
+import { Box, Button, ButtonGroup, Container, Flex, FormatNumber, Group, Heading, HStack, Stack, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router'
 
 export default function Page({ hotel }) {
   const router = useRouter()
-  
+
   const deleteFunction = async () => {
     if (confirm("Are you sure you want to delete this hotel?")) {
       const response = await fetch(`http://localhost/api/hotel/${hotel.id}`, {
@@ -18,10 +19,10 @@ export default function Page({ hotel }) {
   }
 
   return (
-    <div>
-      <h1>{hotel.name}</h1>
-      <p>{hotel.description}</p>
+    <Stack gap={'48px'} margin={'48px'}>
+      <Heading size={'4xl'} textAlign={'center'}>{hotel.name}</Heading>
 
+      {/* Hotel pictures carousel */}
       {hotel.pictures && hotel.pictures.length > 0 && (
         <div>
           {hotel.pictures.sort(function (a, b) {
@@ -32,22 +33,47 @@ export default function Page({ hotel }) {
         </div>
       )}
 
-      <h2>Adresse</h2>
-      <div>
-        <p>{hotel.address}</p>
-        {hotel.address2 ? <p>{hotel.address2}</p> : null}
-        <p>{hotel.zipcode} {hotel.city}, {hotel.country}</p>
-      </div>
+      {/* Content */}
+      <Stack direction={'row'} gap={'24px'}>
+        {/* Left column */}
+        <Stack flexBasis={'70%'} gap={'24px'}>
+          <Text>{hotel.description}</Text>
 
-      <p><strong>Maximum capacity: </strong>{hotel.max_capacity}</p>
-      <p><strong>Price per night: </strong>{hotel.price_per_night}</p>
+          {/* Admin actions box */}
+          <Box borderWidth={'1px'} padding={4}>
+            <Heading size={'lg'}>Admin actions</Heading>
+            <Group>
+              <Button onClick={() => router.push(`/hotel/${hotel.id}/edit`)}>Edit hotel</Button>
+              <Button colorPalette={'red'} onClick={deleteFunction}>Delete hotel</Button>
+            </Group>
+          </Box>
+        </Stack>
 
-      <aside>
-        <h2>Admin actions</h2>
-        <button onClick={() => router.push(`/hotel/${hotel.id}/edit`)}>Edit hotel</button>
-        <button onClick={ deleteFunction }>Delete hotel</button>
-      </aside>
-    </div>
+        {/* Right column */}
+        <Stack flexBasis={'30%'} gap={'24px'}>
+          {/* Adress box */}
+          <Box borderWidth={'1px'} padding={'12px'}>
+            <Heading size={'lg'}>Address</Heading>
+            <Text>{hotel.address}</Text>
+            {hotel.address2 ?? <Text>{hotel.address2}</Text>}
+            <Text>{hotel.zipcode} {hotel.city}, {hotel.country}</Text>
+          </Box>
+
+          {/* Capacity & Price box */}
+          <Stack gapY={'12px'} borderWidth={'1px'} padding={'12px'}>
+            <Container fluid padding={0}>
+              <Heading size={'lg'}>Maximum capacity</Heading>
+              <FormatNumber value={hotel.max_capacity}></FormatNumber>
+            </Container>
+
+            <Container fluid padding={0}>
+              <Heading size={'lg'}>Price per night</Heading>
+              <FormatNumber value={hotel.price_per_night} style='currency' currency='USD'></FormatNumber>
+            </Container>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   )
 }
 
