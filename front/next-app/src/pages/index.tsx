006ccf1892e0ac4e2693121bfe6369b6d6cb0260
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-12-07 23:36:28 +0100

    [Front] Homepage - Improved layout & added create button

--- front/next-app/src/pages/index.tsx
+++ front/next-app/src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Hotel } from "../types/types"
-import { Button, Card, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import { Button, Card, Flex, Heading, Text, Image, Center, Stack, Container } from "@chakra-ui/react";
 
 export default function Home() {
 
@@ -56,43 +56,50 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
-        <title>Hotels</title>
+        <title>Hotels list</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Heading size={'6xl'} as={'h1'}>Hotels</Heading>
+      <Center>
+          <Stack gap={'48px'} margin={'48px'} width={'8xl'}>
+            <Heading size={'6xl'} textAlign={'center'}>Hotels</Heading>
 
-        <Flex gap={'24px'} justifyContent={'center'} flexWrap={'wrap'} margin={'48px'}>
-          {hotels.data.map((hotel) => 
-            <Card.Root maxW="sm" overflow="hidden" key={hotel.id}>
-              {hotel.pictures && hotel.pictures.length > 0 && (
-                <Image
-                  src={hotel.pictures[0].file_path}
-                  alt={hotel.name}
-                  height={'250px'}
-                />
+            <Center>
+              <Link href={'/hotel/new'}>
+                <Button size={'2xl'} width={'fit-content'}>Create new hotel</Button>
+              </Link>
+            </Center>
+
+            <Flex gap={'24px'} justifyContent={'center'} flexWrap={'wrap'}>
+              {hotels.data.map((hotel) => 
+                <Card.Root maxW="sm" overflow="hidden" key={hotel.id}>
+                  {hotel.pictures && hotel.pictures.length > 0 && (
+                    <Image
+                      src={hotel.pictures[0].file_path}
+                      alt={hotel.name}
+                      height={'250px'}
+                    />
+                  )}
+                  <Card.Body gap="2">
+                    <Card.Title>{hotel.name}</Card.Title>
+                    <Card.Description>
+                      <Text lineClamp={2}>{hotel.description}</Text>
+                    </Card.Description>
+                    <Text textStyle="md" fontWeight="medium" letterSpacing="tight" mt="2">
+                      ${hotel.price_per_night} per night
+                    </Text>
+                  </Card.Body>
+                  <Card.Footer gap="2">
+                    <Link href={`/hotel/${hotel.id}`}><Button variant="solid">See details</Button></Link>
+                    <Link href={`/hotel/${hotel.id}/edit`}><Button variant="ghost">Edit</Button></Link>
+                  </Card.Footer>
+                </Card.Root>
               )}
-              <Card.Body gap="2">
-                <Card.Title>{hotel.name}</Card.Title>
-                <Card.Description>
-                  <Text lineClamp={2}>{hotel.description}</Text>
-                </Card.Description>
-                <Text textStyle="md" fontWeight="medium" letterSpacing="tight" mt="2">
-                  ${hotel.price_per_night} per night
-                </Text>
-              </Card.Body>
-              <Card.Footer gap="2">
-                <Link href={`/hotel/${hotel.id}`}><Button variant="solid">See details</Button></Link>
-                <Link href={`/hotel/${hotel.id}/edit`}><Button variant="ghost">Edit</Button></Link>
-              </Card.Footer>
-            </Card.Root>
-          )}
-        </Flex>
-      </main>
-
-    </div>
+            </Flex>
+          </Stack>
+        </Center>
+    </Container>
   );
 }
