a74fcfdb9e1acb4a10b432c648987461a3d53499
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-12-07 20:14:59 +0100

    [Front] Creation form with chakra

--- front/next-app/src/pages/hotel/new.tsx
+++ front/next-app/src/pages/hotel/new.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import styles from '../../styles/Form.module.css'
+import { useState } from 'react';
+import { Alert, Badge, Box, Breadcrumb, Button, Center, Container, Field, FileUpload, Heading, Input, InputGroup, List, NumberInput, Stack, Text, Textarea } from '@chakra-ui/react';
+import { LuFileImage } from 'react-icons/lu';
 
 export default function Page() {
   const router = useRouter()
@@ -72,11 +73,13 @@ export default function Page() {
       setErrors([]);
 
       // Handle pictures
-      const pictures = formData.getAll('pictures');
+      const allPictures = formData.getAll('pictures');
+      // Filter out empty files (FileUpload creates empty files when no selection is made)
+      const pictures = allPictures.filter((file: File) => file.size > 0);
       let hasErrors = false;
       for (let index = 0; index < pictures.length; index++) {
         const element = pictures[index];
-        
+
         const pictureFormData = new FormData();
         pictureFormData.append('picture', element);
         pictureFormData.append('index', index.toString());
@@ -96,13 +99,14 @@ export default function Page() {
       }
 
       // Handle pictures errors
-      if(hasErrors){
+      if (hasErrors) {
         // If there were at least one error uploading pictures, the created hotel should be deleted to avoid confusion
-        fetch("http://localhost/api/hotel/"+createdHotel.id, {
+        fetch("http://localhost/api/hotel/" + createdHotel.id, {
           method: "DELETE"
         });
       } else {
         // Redirect to the created hotel's details page
+        // FIXME Created hotel page returns 404 (but manually reloading said page works)
         router.push(`/hotel/${createdHotel.id}`);
       }
 
@@ -115,58 +119,186 @@ export default function Page() {
   };
 
   return (
-    <div>
-      <h1>Register a new hotel</h1>
-      <p>All fields are required unless stated otherwise</p>
+    <Center>
+      <Stack gap={'48px'} margin={'48px'} width={'8xl'}>
+        {/* Hotel name & breadcrumb */}
+        <Container gapY={'12px'}>
+          <Heading size={'4xl'} textAlign={'center'}>Create a new hotel</Heading>
 
-      {/* Validation errors */}
-      {errors && errors.length > 0 && (
-        <div>
-          {errors.map((message, index) => (
-            <p key={index} style={{ color: 'red', margin: '6px 0' }}>{message}</p>
-          ))}
-        </div>
-      )}
+          <Center>
+            <Breadcrumb.Root>
+              <Breadcrumb.List>
+                <Breadcrumb.Item>
+                  <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+                </Breadcrumb.Item>
+                <Breadcrumb.Separator />
+                <Breadcrumb.Item>
+                  <Breadcrumb.CurrentLink>Create hotel</Breadcrumb.CurrentLink>
+                </Breadcrumb.Item>
+              </Breadcrumb.List>
+            </Breadcrumb.Root>
+          </Center>
+        </Container>
 
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <label htmlFor="name">Hotel name</label>
-        <input type="text" name="name" placeholder="The Cloud One Hotel New York-Downtown" required />
+        {/* Content & Form */}
+        <Stack gapY={'24px'}>
+          <Text>All fields are required unless stated otherwise</Text>
 
-        <label htmlFor="address">Address (number, street)</label>
-        <input type="text" name="address" placeholder="133 Greenwich St" required />
+          {/* Validation errors */}
+          {errors && errors.length > 0 && (
+            <Alert.Root status="error">
+              <Alert.Indicator />
+              <Alert.Content>
+                <Alert.Title fontWeight={'bold'}>Hotel creation {errors.length > 1 ? 'errors' : 'error'}</Alert.Title>
+                <Alert.Description>
+                  <List.Root>
+                    {errors.map((message, index) => (
+                      <List.Item key={index}>{message}</List.Item>
+                    ))}
+                  </List.Root>
+                </Alert.Description>
+              </Alert.Content>
+            </Alert.Root>
+          )}
 
-        <label htmlFor="address2">Complementary address (optional)</label>
-        <input type="text" name="address2" placeholder="" />
+          {/* Fields */}
+          <Box borderWidth={'1px'} padding={'24px'}>
+            <form onSubmit={handleSubmit}>
+              <Stack gapY={'24px'}>
+                <Field.Root required>
+                  <Field.Label>
+                    Hotel name
+                  </Field.Label>
+                  <Input required name='name' type='text' placeholder="The Cloud One Hotel New York-Downtown" />
+                  <Field.HelperText>May be up to 255 characters maximum</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="zipcode">Zipcode</label>
-        <input type="text" name="zipcode" placeholder="NY 10006" required />
+                <Field.Root required>
+                  <Field.Label>
+                    Address
+                  </Field.Label>
+                  <Input required name='address' type='text' placeholder="133 Greenwich St" />
+                  <Field.HelperText>Number and street name</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="city">City</label>
-        <input type="text" name="city" placeholder="New York" required />
+                <Field.Root>
+                  <Field.Label>
+                    Complementary address
+                    <Field.RequiredIndicator
+                      fallback={
+                        <Badge size="sm" variant="surface">
+                          Optional
+                        </Badge>
+                      } />
+                  </Field.Label>
+                  <Input name='address2' type='text' />
+                </Field.Root>
 
-        <label htmlFor="country">Country</label>
-        <input type="text" name="country" placeholder="United States of America" required />
+                <Field.Root required>
+                  <Field.Label>
+                    Zipcode
+                  </Field.Label>
+                  <Input required name='zipcode' type='text' placeholder="NY 10006" />
+                  <Field.HelperText>May be up to 20 characters maximum, may contain numbers, letters and spaces</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="latitude">Latitude</label>
-        <input type="number" name="latitude" placeholder="40.70967754745341" step="0.00000000000001" required />
+                <Field.Root required>
+                  <Field.Label>
+                    City
+                  </Field.Label>
+                  <Input required name='city' type='text' placeholder="New York" />
+                </Field.Root>
 
-        <label htmlFor="longitude">Longitude</label>
-        <input type="number" name="longitude" placeholder="-74.01277611534421" step="0.00000000000001" required />
+                <Field.Root required>
+                  <Field.Label>
+                    Country
+                  </Field.Label>
+                  <Input required name='country' type='text' placeholder="United States of America" />
+                </Field.Root>
 
-        <label htmlFor="description">Description</label>
-        <textarea name="description" id="description" required></textarea>
+                <Field.Root required>
+                  <Field.Label>
+                    Latitude
+                  </Field.Label>
+                  <NumberInput.Root required name='latitude' step={0.00000000000001} min={-90} max={90}>
+                    <NumberInput.Control />
+                    <NumberInput.Input placeholder='40.70967754745341' />
+                  </NumberInput.Root>
+                  <Field.HelperText>Must be between -90 and 90, may be decimal (if so, use the dot and not the comma). Example : 40.70967754745341</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="max_capacity">Maximum capacity</label>
-        <input type="number" name="max_capacity" placeholder="200" required />
+                <Field.Root required>
+                  <Field.Label>
+                    Longitude
+                  </Field.Label>
+                  <NumberInput.Root required name='longitude' step={0.00000000000001} min={-180} max={180}>
+                    <NumberInput.Control />
+                    <NumberInput.Input placeholder='-74.01277611534421' />
+                  </NumberInput.Root>
+                  <Field.HelperText>Must be between -180 and 180, may be decimal (if so, use the dot and not the comma). Example : -74.01277611534421</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="price_per_night">Price per night (in USD)</label>
-        <input type="number" name="price_per_night" placeholder="250" required />
+                <Field.Root required>
+                  <Field.Label>
+                    Description
+                  </Field.Label>
+                  <Textarea required name='description' maxLength={5000} placeholder="Describe the hotel in less than 5000 characters..." />
+                  <Field.HelperText>May be up to 5000 characters maximum</Field.HelperText>
+                </Field.Root>
 
-        <label htmlFor="pictures">Upload pictures (multiple images can be added at once)</label>
-        <input type="file" name="pictures" id="pictures" multiple accept="image/*" />
+                <Field.Root required>
+                  <Field.Label>
+                    Maximum capacity
+                  </Field.Label>
+                  <NumberInput.Root required name='max_capacity' defaultValue="1" step={1} min={1} max={200}>
+                    <NumberInput.Control />
+                    <NumberInput.Input /*placeholder='100'*/ /> {/* Note : Unfortunately the default value takes over the placeholder, and is required as an empty value causes the input to be considered invalid */}
+                  </NumberInput.Root>
+                  <Field.HelperText>Must be an integer between 1 and 200</Field.HelperText>
+                </Field.Root>
 
-        <button type="submit">Create hotel</button>
-      </form>
-    </div>
+                <Field.Root required>
+                  <Field.Label>
+                    Price per night
+                  </Field.Label>
+                  <InputGroup startAddon="$" endAddon="USD per night">
+                    <NumberInput.Root required name='price_per_night' min={0} step={0.01}>
+                      <NumberInput.Control />
+                      <NumberInput.Input placeholder='250' />
+                    </NumberInput.Root>
+                  </InputGroup>
+                  <Field.HelperText>In USD, must be positive, may be decimal (if so, use the dot and not the comma)</Field.HelperText>
+                </Field.Root>
+
+                <Field.Root>
+                  <Field.Label>
+                    Upload pictures
+                    <Field.RequiredIndicator
+                      fallback={
+                        <Badge size="sm" variant="surface">
+                          Optional
+                        </Badge>
+                      }
+                    />
+                  </Field.Label>
+                  <FileUpload.Root maxFiles={10} accept="image/*" name="pictures">
+                    <FileUpload.HiddenInput />
+                    <FileUpload.Trigger asChild>
+                      <Button variant="outline" size="sm">
+                        <LuFileImage /> Upload pictures
+                      </Button>
+                    </FileUpload.Trigger>
+                    <FileUpload.List showSize clearable />
+                  </FileUpload.Root>
+                  <Field.HelperText>Maximum size : 2MB. You can upload up to 10 pictures at once. Each picture will be added to the slideshow in the order they are selected.</Field.HelperText>
+                </Field.Root>
+
+                <Button type="submit" size={'xl'}>Create hotel</Button>
+              </Stack>
+            </form>
+          </Box>
+        </Stack>
+      </Stack>
+    </Center>
   )
 }
